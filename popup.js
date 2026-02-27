27de6c680eebe381cb60d890c780731c7c86a7af
Tanisha <tthompsonjones8771@gmail.com>
2026-02-27 16:05:02 -0500

    add another console log

--- popup.js
+++ popup.js
@@ -2,7 +2,8 @@ console.log("pop up running...")
 async function popupMsg() {
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-
+    console.log(tab)
+    console.log([tab])
 
     const layoutBtn = document.querySelector("btn")
     layoutBtn.addEventListener('click', (event) => {
